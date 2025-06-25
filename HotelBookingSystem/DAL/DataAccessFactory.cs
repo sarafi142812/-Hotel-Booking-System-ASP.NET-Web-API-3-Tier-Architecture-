7b3cb8d488f2a48490d9bfb5d56e9f5415cca710
Samiul Alam Rafi <sarafi7814@gmail.com>
2025-06-25 20:29:48 +0600

    web api project

---
+++ HotelBookingSystem/DAL/DataAccessFactory.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.EF;
+using DAL.Interfaces;
+using DAL.Repos;
+
+namespace DAL
+{
+    public class DataAccessFactory
+    {
+        public static IRepo<Guest, int, Guest> GuestDataAccess()
+        {
+            return new GuestRepo();
+        }
+
+        public static IRepo<Room, int, Room> RoomDataAccess()
+        {
+            return new RoomRepo();
+        }
+
+        public static IRepo<Booking, int, Booking> BookingDataAccess()
+        {
+            return new BookingRepo();
+        }
+    }
+}
---
+++ HotelBookingSystem/DAL/Interfaces/IRepo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Interfaces
+{
+    public interface IRepo<CLASS, ID, RETURN>
+    {
+        List<CLASS> GetAll();
+        RETURN GetById(ID id);
+        RETURN Add(CLASS obj);
+        RETURN Update(CLASS obj);
+        bool Delete(ID id);
+    }
+}
